de5363095fbea4190462616b6610076eb9627b2c
Pierre-Henry Fröhring <contact@phfrohring.com>
2025-01-18 15:25:57 +0100

    Added user-format-all-js-code-blocks to prettify JS in code blocks (elisp/org-patches.el)

--- elisp/org-patches.el
+++ elisp/org-patches.el
@@ -1,4 +1,4 @@
-;; tangled files
+;; Property drawers are removed after tangling
 
 (defun user--org-remove-property-drawers ()
   "After tangling, remove all :PROPERTIES: drawers."
@@ -13,8 +13,9 @@
       (set-buffer-modified-p modified-p))))
 (add-hook 'org-babel-post-tangle-hook #'user--org-remove-property-drawers)
 
-
-;; org-babel-spec-to-string
+;; org-babel-spec-to-string is fixed
+;; Like the original version
+;; but do not insert useless new line.
 
 (defun org-babel-spec-to-string (spec)
   "Insert SPEC into the current file.
@@ -56,5 +57,90 @@ that the appropriate major-mode is set.  SPEC has the form:
       (funcall insert-comment
                (org-fill-template
                 org-babel-tangle-comment-format-end link-data)))))
+
+;; All JS code blocks have been formatted
+;; - λ()
+;;   - point :≡ point-min()
+;;   - search-next-code-block() ≡
+;;     - error[msg] ⇒ message(msg)
+;;     - nil ⇒ ■
+;;     - pair[start end] ⇒
+;;       - format-code extract-code(start end) ≡
+;;         - error[msg] ⇒ message(msg)
+;;         - formatted-code ⇒ replace-code(start end formatted-code)
+;;       - point :≡ end
+;;       - λ()
+
+(defun user-format-all-js-code-blocks ()
+  (interactive)
+  (when (not (executable-find "prettier"))
+    (user-error "prettier has not been found in PATH"))
+  (save-excursion
+    (goto-char (point-min))
+    (pcase (user--next-code-block "js")
+      ('nil
+       (message "All js code blocks have been formatted using prettier"))
+      (`(:error ,msg)
+       (user-error msg))
+      (`(,start ,end)
+       (pcase (user--format-code (user--extract-code start end)
+                                 (get-buffer-create "*Formatting errors*"))
+         (`(:error ,msg)
+          (message "Code block starting at %s has not been formatted.\n%s" start msg)
+          (with-restriction end (point-max)
+            (user-format-all-js-code-blocks)))
+         (formatted-code
+          (replace-code start end formatted-code)
+          (goto-char start)
+          (pcase (user--next-code-block "js")
+            (`(,start ,end)
+             (with-restriction end (point-max)
+               (user-format-all-js-code-blocks))))))))))
+
+(defun user--next-code-block (tag)
+  (save-excursion
+    (let ((case-fold-search t) begin-re end-re start)
+      (setq begin-re
+            (rx-to-string `(seq bol (0+ " ") "#+begin_src" (1+ " ") (literal ,tag))))
+      (pcase (search-forward-regexp begin-re nil t)
+        ('nil nil)
+        (_
+         (forward-line 1)
+         (setq start (point))
+         (setq end-re (rx-to-string '(seq bol (0+ " ") "#+end_src" (0+ " "))))
+         (pcase (search-forward-regexp end-re nil t)
+           ('nil
+            (list :error (format "start of code block at %s has no matching end")))
+           (_
+            (beginning-of-line)
+            (list start (point)))))))))
+
+(defun user--extract-code (start end)
+  (buffer-substring-no-properties start end))
+
+(defun user--replace-code (start end code)
+  (save-excursion
+    (kill-region start end)
+    (goto-char start)
+    (insert code)))
+
+(defun user--format-code (code error-buffer)
+  (let (return-code)
+    (with-temp-buffer
+      (insert code)
+      (setq return-code
+            (shell-command-on-region
+             (point-min)
+             (point-max)
+             "prettier --stdin-filepath tmp.js"
+             (current-buffer) t
+             error-buffer))
+      (pcase return-code
+        (0
+         (buffer-substring-no-properties (point-min) (point-max)))
+        (_
+         (list :error (format "Formatting error. See buffer %s" (buffer-name error-buffer))))))))
+
+;; provide
 
 (provide 'org-patches)
